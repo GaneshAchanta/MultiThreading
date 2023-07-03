8bf84447b52721411b2dd59a740e824d3c17ecaf
Siva Rama Ganesh Achanta <124123785+GaneshAchanta@users.noreply.github.com>
2023-07-03 16:58:32 +0530

    Add files via upload

---
+++ SleepDemo.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class SleepDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("P");
+		Thread.sleep(3000);
+		System.out.println("A");
+		Thread.sleep(3000);
+		System.out.println("V");
+		Thread.sleep(3000);
+		System.out.println("A");
+		Thread.sleep(3000);
+		System.out.println("N");
+
+
+	}
+
+}
