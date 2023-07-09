639a2c6bae01c8f4c04e6e6b585bedb3cea1e2c9
Siva Rama Ganesh Achanta <124123785+GaneshAchanta@users.noreply.github.com>
2023-07-09 12:58:11 +0530

    Add files via upload

---
+++ ThreadYieldDemo.java
@@ -0,0 +1,26 @@
+package basic;
+
+class YieldThread extends Thread{
+	public void run()	{
+		for(int i=0;i<5;i++){
+			Thread.yield();
+			System.out.println("child thread");
+		}
+	}
+}
+
+public class ThreadYieldDemo {
+
+	public static void main(String[] args) {
+		YieldThread y=new YieldThread();
+		Thread t=new Thread(y);	
+		t.setPriority(10);
+		t.start();
+		for(int i=0;i<5;i++){
+				System.out.println("main thread");
+		}
+
+
+	}
+
+}
